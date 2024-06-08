1c4ff86259e1385831c4f4f5210d152d8b37f968
Saayonee Dhepe <saayonee05@gmail.com>
2024-06-08 16:36:22 +0530

    Components
    Desc

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import HeroPage from "./components/HeroPage";
+import Features from "./components/Features";
+import Gallery from "./components/Gallery";
+import Testimonials from "./components/Testimonials";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
@@ -6,6 +12,12 @@ export default function Home() {
       <div>
         Hello World
       </div>
+      <HeroPage />
+      <Features />
+      <Gallery />
+      <Testimonials />
+      <Pricing />
+      <Contact />
     </main>
   );
 }
